6b657b9006c5ec963b2c712571025a0dbd85e929
liu xing <chn.liuxing@gmail.com>
2018-06-05 11:18:15 +0800

    feat: category pagination

--- routes/posts.js
+++ routes/posts.js
@@ -6,11 +6,19 @@ module.exports = {
   async index (ctx, next) {
     const pageSize = 5
     const currentPage = parseInt(ctx.query.page) || 1
-    const allPostsCount = await PostModel.count()
+    const cname = ctx.query.c
+    let cid
+    if (cname) {
+      const cateogry = await CategoryModel.findOne({ name: cname })
+      cid = cateogry._id
+    }
+    const query = cid ? { category: cid } : {}
+    const allPostsCount = await PostModel.find(query).count()
     const pageCount = Math.ceil(allPostsCount / pageSize)
     const pageStart = currentPage - 2 > 0 ? currentPage - 2 : 1
     const pageEnd = pageStart + 4 >= pageCount ? pageCount : pageStart + 4
-    const posts = await PostModel.find({}).skip((currentPage - 1) * pageSize).limit(pageSize)
+    const posts = await PostModel.find(query).skip((currentPage - 1) * pageSize).limit(pageSize)
+    const baseUrl = cname ? `${ctx.path}?c=${cname}&page=` : `${ctx.path}?page=`
     await ctx.render('index', {
       title: 'JS之禅',
       posts,
@@ -19,7 +27,8 @@ module.exports = {
       allPostsCount,
       pageCount,
       pageStart,
-      pageEnd
+      pageEnd,
+      baseUrl
     })
   },
 
@@ -41,7 +50,10 @@ module.exports = {
   },
   async show (ctx, next) {
     const post = await PostModel.findById(ctx.params.id)
-      .populate({ path: 'author', select: 'name' })
+      .populate([
+        { path: 'author', select: 'name' },
+        { path: 'category', select: ['title', 'name'] }
+      ])
     const comments = await CommentModel.find({ postId: ctx.params.id })
       .populate({ path: 'from', select: 'name' })
     await ctx.render('post', {
@@ -54,6 +66,7 @@ module.exports = {
   async edit (ctx, next) {
     if (ctx.method === 'GET') {
       const post = await PostModel.findById(ctx.params.id)
+      const categories = await CategoryModel.find({})
       if (!post) {
         throw new Error('文章不存在')
       }
@@ -62,14 +75,16 @@ module.exports = {
       }
       await ctx.render('edit', {
         title: '更新文章',
-        post
+        post,
+        categories
       })
       return
     }
-    const { title, content } = ctx.request.body
+    const { title, content, category } = ctx.request.body
     await PostModel.findByIdAndUpdate(ctx.params.id, {
       title,
-      content
+      content,
+      category
     })
     ctx.flash = { success: '更新文章成功' }
     ctx.redirect(`/posts/${ctx.params.id}`)
